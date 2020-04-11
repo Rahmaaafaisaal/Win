831d24d85ccf8b30824054d0e5edc7c63c505ab6
RahmaFaisal <rahfaisaaal@gmail.com>
2020-04-11 22:48:25 +0200

    home updates

--- public/js/winHome.js
+++ public/js/winHome.js
@@ -1,16 +1,22 @@
 const signBtn=document.getElementById('signInbtn');
 const welcomeDiv=document.getElementById('welcomeDiv');
 const registrationDiv=document.getElementById('registrationDiv');
-
-
+const body=document.getElementsByTagName('body')
+const HomePage=document.getElementById('HomePage');
 function handleSignIn(e)
 {
     welcomeDiv.style.display="none"
+    body[0].classList.remove('bk')
     registrationDiv.style.display="block"
     
 }
 
-
+function goToHome(){
+    body[0].classList.add('bk')
+    welcomeDiv.style.display="block"
+    registrationDiv.style.display="none"
+}
 
 
 signBtn.addEventListener('click',handleSignIn)
+HomePage.addEventListener('click',goToHome)
